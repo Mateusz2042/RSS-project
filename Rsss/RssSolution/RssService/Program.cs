30953bc043e26e5a9b23134fc75a6a6199e2dbf3
kubalone <kubalone@gmail.com>
2017-04-01 16:38:22 +0200

    Service added

--- Rsss/RssSolution/RssService/Program.cs
+++ Rsss/RssSolution/RssService/Program.cs
@@ -5,19 +5,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Topshelf;
 namespace RssService
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            RssContext db = new RssContext();
-            DbWriter.Write();
+            HostFactory.Run(z =>
+            {
+                z.Service<ServiceCore>(s =>
+                {
+                    s.ConstructUsing(c => new ServiceCore());
+                    s.WhenStarted(c => c.Start());
+                    s.WhenStopped(c => c.Stop());
+                });
+                z.RunAsLocalSystem();
+                z.StartAutomatically();
 
-            Console.WriteLine("Database write successes !!!");
-            Console.ReadKey();
+
+            });
         }
     }
 }
---
+++ Rsss/RssSolution/RssService/ServiceCore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rsss.Database;
+using Rsss.DatabaseWriter;
+//using System.Threading;
+using System.Timers;
+namespace RssService
+{
+    class ServiceCore
+    {
+        readonly Timer _timer;
+        public ServiceCore()
+        {
+            RssContext db = new RssContext();
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => DbWriter.Write();
+
+
+
+            Console.WriteLine("Database write successes !!!");
+            //  Console.ReadKey();
+        }
+        public void Start()
+        {
+            _timer.Start();
+            Console.WriteLine("Service started");
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+            Console.WriteLine("Service stopped");
+        }
+      
+    }
+}
